3ccc190f1e685fad144b4343ea213b92e0be7347
kxGeou <grzegorzm.work@gmail.com>
2025-03-10 22:51:41 +0100

    in

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,9 @@
+
+export default function Dashboard() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-700 to-slate-800 flex justify-center items-center">
+        <div className="flex items-start max-w-[1400px] py-12 px-2 min-h-screen w-full md:px-8 lg:px-0">
+        </div>
+    </div>
+  )
+}
